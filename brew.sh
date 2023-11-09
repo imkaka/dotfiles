9582ae851818cae5b3b47b8f7ea6ab7919af6d83
Anil Khatri <anilkhatri.dev@gmail.com>
2023-11-09 15:59:36 +0530

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -2,6 +2,7 @@
 
 # Install Brew Packages
 brew install pyenv
+brew install asdf
 # brew install python
 brew install tree
 
@@ -30,3 +31,6 @@ brew install --cask font-fira-code
 brew install fish
 brew install starship
 brew install --cask fig
+
+brew install kubectl
+brew instal kubectx
